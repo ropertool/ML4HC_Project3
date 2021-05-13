87fb43e3bba632fddad9db3ac9a8376ffc86ede1
Karol <karolbo1@o2.pl>
2021-05-13 18:32:13 +0200

    bugs fixed

--- LSTM_classification.py
+++ LSTM_classification.py
@@ -1,7 +1,6 @@
 from preprocessing import get_data
 from gensim.models import Word2Vec
-import numpy as np
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+# import numpy as np
 from tensorflow.keras.layers import Dense, LSTM, Embedding
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.initializers import Constant
@@ -14,24 +13,24 @@ def clead_data(labels, corpus):
             corpus.pop(i)
             labels.pop(i)
     assert(len(labels) == len(corpus))
-    return corpus, labels
+    return labels, corpus
 
 
 # Get preprocessed data
-print('Preprocessing data...', end=' ')
+print('Preprocessing data...', end=' ', flush=True)
 labels, corpus = get_data('train')
 labels_valid, corpus_valid = get_data('dev')
 # labels_test, corpus_test = get_data('test')
-print('done')
+print('done', flush=True)
 
 # Clean the data
-print('Cleaning data...', end=' ')
+print('Cleaning data...', end=' ', flush=True)
 labels, corpus = clead_data(labels, corpus)
 labels_valid, corpus_valid = clead_data(labels_valid, corpus_valid)
 # labels_test, corpus_test = clead_data(labels_test, corpus_test)
-print('done')
+print('done', flush=True)
 
-classes = len(np.unique(labels))
+# classes = len(np.unique(labels))
 
 # Load word2vec model
 w2v = Word2Vec.load("trained_models/test.model")
@@ -48,11 +47,11 @@ model.add(Embedding(input_dim=w2v.wv.vectors.shape[0], output_dim=w2v.wv.vectors
                     embeddings_initializer=Constant(w2v.wv.vectors), trainable=False, mask_zero=True))
 model.add(LSTM(units=w2v.wv.vectors.shape[1]))
 model.add(Dense(units=64, activation='relu'))
-model.add(Dense(units=classes, activation='softmax'))
+model.add(Dense(units=6, activation='softmax'))
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
 
 # Train
-model.fit(x=corpus_valid, y=labels_valid,
+model.fit(x=corpus, y=labels,
           validation_data=(corpus_valid, labels_valid), epochs=5, batch_size=1)
 
 model.save('LSTM_model.h5')
